075107eea877da9a46eb1d731f20414f90d7cc55
Mathias <mathiasdcm@gmail.com>
2020-11-22 12:07:39 +0100

    Score moyen remplace l'accuracy dans desicion tree classifier

--- projet_ghready.py
+++ projet_ghready.py
@@ -31,7 +31,6 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-#helooooo
 
 colors = {
     "graphBackground": "#F5F5F5",
